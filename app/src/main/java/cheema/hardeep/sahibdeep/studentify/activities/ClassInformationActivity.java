7c9160b52872c59646aa66a639bf4df16b21ff7e
Hardeep Singh <hardeep@cazisoft.com>
2019-08-10 12:20:51 -0400

    Setup Project

---
+++ app/src/main/java/cheema/hardeep/sahibdeep/studentify/activities/ClassInformationActivity.java
@@ -0,0 +1,16 @@
+package cheema.hardeep.sahibdeep.studentify.activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import cheema.hardeep.sahibdeep.studentify.R;
+
+public class ClassInformationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_class_information);
+    }
+}
---
+++ app/src/main/java/cheema/hardeep/sahibdeep/studentify/activities/TasksActivity.java
@@ -0,0 +1,16 @@
+package cheema.hardeep.sahibdeep.studentify.activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import cheema.hardeep.sahibdeep.studentify.R;
+
+public class TasksActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tasks);
+    }
+}
